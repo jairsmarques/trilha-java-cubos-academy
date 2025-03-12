3d161b42da04efd0158b7a7231d08e6a7f088a98
Jair Marques <jairsmarques1126@gmail.com>
2025-03-12 12:20:35 -0300

    exemplos de variáveis e constantes

---
+++ ola-mundo/src/VariaveisEConstantes.java
@@ -0,0 +1,19 @@
+public class VariaveisEConstantes {
+
+    public static void main(String[] args) {
+
+        String txt = "Jair de Sousa";
+        System.out.println(txt);
+
+        txt = "Cubos Academy";
+
+        //txt = 123; -> erro, tipo diferente do declarado
+
+        System.out.println(txt);
+
+        final String txt2 = "Minha primeira constate";
+        //txt2 = "Deu erro"; -> não consigo alterar uma constante
+
+        System.out.println(txt2);
+    }
+}
