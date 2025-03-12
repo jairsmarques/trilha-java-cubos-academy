da898060f721b744b78dbb4346ceddfdc68a108f
Jair Marques <jairsmarques1126@gmail.com>
2025-03-12 12:49:24 -0300

    exemplo da estrutura condicional switch

---
+++ ola-mundo/src/EstruturaDeCondicaoSwitch.java
@@ -0,0 +1,19 @@
+public class EstruturaDeCondicaoSwitch {
+
+    public static void main(String[] args) {
+
+        int numero3 = 18;
+
+        switch (numero3) {
+            case 18 :
+                System.out.println("O número é 18");
+                break;
+            case 17 :
+                System.out.println("O número é 17");
+                break;
+            default:
+                System.out.println("O número é desconhecido");
+               break;
+        }
+    }
+}
