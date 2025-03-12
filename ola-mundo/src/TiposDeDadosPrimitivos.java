ec20163aac3f800e6a2b9557ed1714006b807d66
Jair Marques <jairsmarques1126@gmail.com>
2025-03-12 12:31:27 -0300

    exemplos de tipos primitivos

---
+++ ola-mundo/src/TiposDeDadosPrimitivos.java
@@ -0,0 +1,25 @@
+public class TiposDeDadosPrimitivos {
+
+    public static void main(String[] args) {
+        // Variável e tipo de dado texto
+        String txt = "Jair de Sousa";
+        System.out.println(txt);
+
+        txt = "Cubos Academy";
+        System.out.println(txt);
+
+        final String txt2 = "Minha primeira constate";
+        System.out.println(txt2);
+
+        //Números
+        // números inteiros
+        int numeroInteiro = 12;
+        System.out.println(numeroInteiro);
+        //números com casas decimais
+        double numeroDecimal = 12.5;
+        System.out.println(numeroDecimal);
+        //Lógico
+        boolean verdadeiroOuFalse = true;
+        System.out.println(verdadeiroOuFalse);
+    }
+}
